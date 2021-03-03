d3264060dbb148829af47b3cabe7896126618511
Neythen <zcqsntr@ucl.ac.uk>
2021-03-03 18:47:46 +0000

    pi3A

--- backend/paint/views.py
+++ backend/paint/views.py
@@ -19,11 +19,11 @@ IF YOU DON'T KNOW THE ARGUMENTS I SUGGEST READING THE RGB-RPI LIBRARY DOCUMENTAT
 THEN REPLACE THE STRING BELOW WITH THE ARGUMENTS YOU HAVE TO SUPPLY TO RUN THE DEMOS, WITHOUT THE -D OBVIOUSLY
 """
 
-# this line is for my 64x64 matrix using adafruit-hat running on a pi 4
+# this line is for my 64x64 matrix using adafruit-hat running on a pi 4 or a pi 3A
 rpi_rgb_args = "--led-cols=64 --led-rows=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4"
 
-# this line is for my 32x64 matrix using adafruit-hat running on a pi 4
-rpi_rgb_args = "--led-cols=64 --led-rows=32 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4"
+# this line is for my 32x64 matrix using adafruit-hat running on a pi 4 or a pi 3A
+#rpi_rgb_args = "--led-cols=64 --led-rows=32 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4"
 
 
 child = pexpect.spawn("paint/paint " + rpi_rgb_args)
